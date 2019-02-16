ba9277b795377cff2e7c024634f9ac483f8325ff
Keith <kayzee@live.com>
2019-02-16 09:51:41 -0800

    html and js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+// eslint-disable-next-line no-unused-vars
+var hours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+  '8pm',
+];
+/***
+function randNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) +min
+}
+
+var getCookies = document.getElementById('cookies');
+
+var salmonCookies = {
+  render:function() {
+    for(var x = 0; x < hours.length; x++) {
+      var createText = document.createElement('li');
+      var createRandom = Math.floor(Math.random() * 100);
+      createText.textContent = `${hours[x]}: ${createRandom} cookies`;
+      getCookies.appendChild(createText);
+    }
+  }
+};
+salmonCookies.render();
+***/
